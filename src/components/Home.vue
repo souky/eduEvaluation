f5874e3876884b583df015a00c023f6b6ea7d04f
souky <136069803@qq.com>
2017-11-27 10:25:02 +0800

    echarts

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,36 @@
 <template>
+	<div id="home" class="main_body">
+		<div id="sasd" style="width: 600px;height:400px;"></div>
+		asdasd
+	</div>
+	
 </template>
 
 <script>
+export default {
+  data () {
+
+    return {
+      msg: '顶部导航栏',
+    }
+  },
+  mounted:function(){
+  	//判断cookie登陆信息初始化
+  	this.echarts.init(document.getElementById("sasd")).setOption({
+		series: {
+            type: 'pie',
+            data: [
+                {name: 'A', value: 1212},
+                {name: 'B', value: 2323},
+                {name: 'C', value: 1919}
+            ]
+        }
+	});
+  },
+  methods:{
+
+  }
+}
 </script>
 
 <style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+
+
 import Home from '@/components/Home'
 
 Vue.use(Router)
@@ -11,6 +13,11 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home
-    }
+    },
+    {
+        path: '/home',
+        name: 'home',
+        component: Home
+      }
   ]
 })
